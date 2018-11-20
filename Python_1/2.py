981dd2cc163d7ecc75ce9fc242797862751de9f9
zossow <45214400+zossow@users.noreply.github.com>
2018-11-20 22:38:44 +0100

    Create 2.py

---
+++ Python_1/2.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+imie, nazwisko, rokurodzenia = input("Podaj swoje imie, nazwisko i rok urodzenia oddzielone spacja: ").split()
+print("Twoje dane:", imie, nazwisko, rokurodzenia)
