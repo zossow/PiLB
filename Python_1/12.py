1e393778ec54829688f9e53e33f86a109dfc4bf1
zossow <45214400+zossow@users.noreply.github.com>
2018-11-22 17:57:39 +0100

    Update 12.py

--- Python_1/12.py
+++ Python_1/12.py
@@ -4,5 +4,9 @@ X = [[random.randint(1,1000) for a in range(wymiar)] for a in range(wymiar)]
 Y = [[random.randint(1,1000) for b in range(wymiar)] for b in range(wymiar)]
 result = [[X[i][j] + Y[i][j]  for j in range(len(X[0]))] for i in range(len(X))]
 
+print(X)
+print('\n')
+print(Y)
+print('\n')
 for r in result:
     print(r)
